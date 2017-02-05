798bd78eb60c2965ad6de20248e92b4ab433cd77
Giang Dinh <giang.dinh@tiki.vn>
2017-02-06 01:44:39 +0700

    Create module shopthucong

---
+++ addons/shopthucong/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'shopthucong.com',
+    'version': '0.1',
+    'category': 'Point Of Sale',
+    'sequence': 20,
+    'summary': 'Point Of Sale modifications for shopthucong.com',
+    'description': """
+Main Features
+-------------
+* Beautify receipt billing
+    """,
+    'depends': ['point_of_sale'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'qweb': ['static/src/xml/pos.xml'],
+    'website': 'http://www.shopthucong.com',
+}
