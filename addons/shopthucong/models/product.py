1181002814a94855dafca6a52988df3148301cfe
Giang Dinh <giang.dinh@tiki.vn>
2017-03-01 00:41:27 +0700

    Update product image url

--- addons/shopthucong/models/product.py
+++ addons/shopthucong/models/product.py
@@ -9,12 +9,10 @@ class ProductProduct(models.Model):
 
     @api.one
     @api.depends('company_id.website', 'image_name', 'company_id')
-    def _get_image_uri(self):
-        # print ' in _get_image uri'
-        # print self.name, self.image_name, self.company_id.website
+    def _get_image_url(self):
         self.image_url = self.company_id.website + '/image/' + self.image_uri
         self.image_tag = """<img src="%s" height="128" width="128"/>""" % (self.image_url,)
 
     image_uri = fields.Char('Image URI')
-    image_url = fields.Char('Image URL', compute='_get_image_uri', store=False)
-    image_tag = fields.Char('Image tag view', compute='_get_image_uri', store=False)
+    image_url = fields.Char('Image URL', compute='_get_image_url', store=False)
+    image_tag = fields.Char('Image tag view', compute='_get_image_url', store=False)
